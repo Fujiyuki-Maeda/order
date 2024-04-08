4a12b6a7de16fc4342bbc2c6ada9ab5c626b7c41
Fujiyuki-Maeda <goku-maeda@outlook.jp>
2024-04-08 17:37:16 +0900

    scondcommit

--- invoiceproject/settings.py
+++ invoiceproject/settings.py
@@ -24,7 +24,7 @@ except ImportError:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
