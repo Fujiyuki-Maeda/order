8de8df2e1d384e315644e52bf99e58e8d270d4cf
Fujiyuki-Maeda <goku-maeda@outlook.jp>
2024-04-08 17:46:42 +0900

    thridcommit

--- invoiceproject/settings.py
+++ invoiceproject/settings.py
@@ -24,7 +24,7 @@ except ImportError:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
